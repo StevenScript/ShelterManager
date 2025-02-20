22c54f33a42a985366417357ca5a5764e0fba8be
StevenScript <sdn709466@gmail.com>
2025-02-20 13:15:38 -0330

    ADD: Dog and Cat classes
    extends Animal class

---
+++ src/main/java/com/ShelterManager/Cat.java
@@ -0,0 +1,10 @@
+package com.ShelterManager;
+
+/**
+ * Represents a Cat in the animal shelter.
+ */
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/com/ShelterManager/Dog.java
@@ -0,0 +1,10 @@
+package com.ShelterManager;
+
+/**
+ * Represents a Dog
+ */
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+}
